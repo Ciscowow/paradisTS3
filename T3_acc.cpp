45adb18a83df62f9941506191da303f5cd8df677
Ciscowow <144804969+Ciscowow@users.noreply.github.com>
2025-03-23 21:23:25 +0800

    Update T3_acc.cpp

--- T3_acc.cpp
+++ T3_acc.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cstdint>
 #include <chrono>
-#include <cstdlib>  // For malloc/free
+#include <cstdlib>  
 
 using namespace std;
 
@@ -11,8 +11,8 @@ struct Pixel {
 };
 
 int main() {
-    const char* infile = "./rgb_image.bmp";  // Input BMP image
-    const char* outfile = "./Grayscale_image_parallel.bmp";  // Output grayscale BMP
+    const char* infile = "./rgb_image.bmp";  
+    const char* outfile = "./Grayscale_image_parallel.bmp";  
 
     ifstream in(infile, ios::binary);
     ofstream out(outfile, ios::binary);
@@ -22,16 +22,14 @@ int main() {
         return 1;
     }
 
-    // Read BMP Header (keep on CPU)
     char Header[54];
     in.read(Header, sizeof(Header));
     out.write(Header, sizeof(Header));
 
-    // Get file size and compute pixel count
     in.seekg(0, ios::end);
     size_t fileSize = in.tellg();
-    in.seekg(54, ios::beg);  // Skip header
-
+    in.seekg(54, ios::beg);  
+    
     size_t pixelCount = (fileSize - 54) / sizeof(Pixel);
     Pixel* pixels = (Pixel*)malloc(pixelCount * sizeof(Pixel));
 
@@ -40,16 +38,13 @@ int main() {
         return 1;
     }
 
-    // Read pixel data
     in.read(reinterpret_cast<char*>(pixels), pixelCount * sizeof(Pixel));
     in.close();
 
-    // Transfer pixel data to GPU
     #pragma acc enter data copyin(pixels[0:pixelCount])
 
     auto start = chrono::high_resolution_clock::now();
 
-    // OpenACC Parallel Grayscale Conversion
     #pragma acc parallel loop present(pixels[0:pixelCount])
     for (size_t i = 0; i < pixelCount; i++) {
         uint8_t gray = static_cast<uint8_t>(0.299 * pixels[i].r + 0.587 * pixels[i].g + 0.114 * pixels[i].b);
@@ -58,16 +53,13 @@ int main() {
 
     auto end = chrono::high_resolution_clock::now();
 
-    // Copy data back from GPU
     #pragma acc exit data copyout(pixels[0:pixelCount])
 
-    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);  // Now in MICROSECONDS
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);  
 
-    // Write modified pixel data
     out.write(reinterpret_cast<char*>(pixels), pixelCount * sizeof(Pixel));
     out.close();
 
-    // Free allocated memory
     free(pixels);
 
     cout << "Total time: " << duration.count() << " microseconds (Parallel)" << endl;
